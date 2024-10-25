8beb93b67f1a54008eaba4aeb785cd60e3ad7761
VernSG <snexmania76@gmail.com>
2024-10-25 14:16:20 +0800

    fix(index): add fragment

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,12 +4,10 @@ import NavigationMenuDemo from "@/components/ui/navbar";
 
 export default function Home() {
   return (
-    <div>
+    <>
       <NavigationMenuDemo />
-      <div className="my-8 p-8">
-        <CarouselPlugin />
-        <Footer />
-      </div>
-    </div>
+      <CarouselPlugin />
+      <Footer />
+    </>
   );
 }
