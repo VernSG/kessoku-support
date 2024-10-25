b2c6d9e56b07ac2f468cec19b742f4bfd8065f25
VernSG <snexmania76@gmail.com>
2024-10-25 14:07:00 +0800

    fix(index): cover nav with div and delete fragment

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,13 +3,19 @@
 import dynamic from "next/dynamic";
 
 // Mengimpor komponen secara dinamis untuk mencegah SSR jika mereka menggunakan API browser
-const NavigationMenuDemo = dynamic(() => import("../components/ui/navbar"), {
-  ssr: false,
-});
+const NavigationMenuDemo = dynamic(
+  () => import("../components/ui/navbar").then((mod) => mod.default),
+  {
+    ssr: false,
+  }
+);
 
-const CarouselPlugin = dynamic(() => import("../components/carousel"), {
-  ssr: false,
-});
+const CarouselPlugin = dynamic(
+  () => import("../components/carousel").then((mod) => mod.default),
+  {
+    ssr: false,
+  }
+);
 
 const Footer = dynamic(() => import("../components/ui/footer"), {
   ssr: false,
@@ -17,12 +23,12 @@ const Footer = dynamic(() => import("../components/ui/footer"), {
 
 export default function Home() {
   return (
-    <>
+    <div>
       <NavigationMenuDemo />
       <div className="my-8 p-8">
         <CarouselPlugin />
         <Footer />
       </div>
-    </>
+    </div>
   );
 }
