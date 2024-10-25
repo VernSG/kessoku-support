5bd13c7ccec9e70053390b62bfcd1dd192b80a57
VernSG <snexmania76@gmail.com>
2024-10-25 14:28:02 +0800

    fix(index): use fallback state

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,21 @@
+import NavigationMenuDemo from "@/components/ui/navbar";
 import CarouselPlugin from "@/components/carousel";
 import Footer from "@/components/ui/footer";
-import NavigationMenuDemo from "@/components/ui/navbar";
 
-export default function Home() {
+type HomeData = {
+  title: string;
+  description: string;
+};
+
+type HomeProps = {
+  data?: HomeData;
+};
+
+export default function Home({ data }: HomeProps) {
+  if (!data) {
+    return <div>Loading...</div>; // Tampilkan state sementara jika data tidak tersedia
+  }
+
   return (
     <div>
       <NavigationMenuDemo />
