6fc00e21388060d49caaba2ee9c6b939810475f3
VernSG <snexmania76@gmail.com>
2024-10-25 13:25:46 +0800

    fix(index): delete footer import (eslint rule)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 import { NavigationMenuDemo } from "../components/ui/navbar";
 import { CarouselPlugin } from "../components/carousel";
-import Footer from "@/components/ui/footer";
 
 export default function Home() {
   return (
