fe0b50d82a6dd5cdb3f26fe602d1b168b164355b
VernSG <snexmania76@gmail.com>
2024-10-25 13:29:44 +0800

    fix(index): delete fragment <>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,11 +3,11 @@ import { CarouselPlugin } from "../components/carousel";
 
 export default function Home() {
   return (
-    <>
+    <div>
       <NavigationMenuDemo />
       <div className="my-8 p-9">
         <CarouselPlugin />
       </div>
-    </>
+    </div>
   );
 }
